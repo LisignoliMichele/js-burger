c55afd1a13b321fefefba7b1b8ab372f9acb1026
Michele Lisignoli <michele@lisignoli.com>
2021-03-05 16:42:04 +0100

    calculate the total price for chosen  ingredients

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 // variables
 var burgerName = document.getElementById("burgerName");
 var calculate = document.getElementById("buttonCalculate");
-
-
+var ingredient = document.getElementsByClassName('ingredient');
+var sum = 0;
 // click
-
 calculate.addEventListener("click",
   function() {
-    alert("funziona")
+    // condition if burgerName is empty
+    if (burgerName.value == "") {
+      alert("Before place your order, you must enter Your burger name!!")
+    }else{
+      // calculate the total price for chosen ingredients
+      for (var i = 0; i < ingredient.length; i++) {
+        if (ingredient[i].checked) {
+          sum += parseInt(ingredient[i].value);
+        }
+      }
+
+    }
   }
 );
-//condition if burgerName is empty
-// if (burgerName.value == "") {
-//   alert("Before place your order, you must enter Your burger name!!")
-// }else{
-//   alert("ok")
-// }
