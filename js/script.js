e8d07ffcabb1ae2a60cc5197b3f08597450dc119
Michele Lisignoli <michele@lisignoli.com>
2021-03-05 16:10:31 +0100

    added Event Listener // condition if burgerName is empty

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// variables
+var burgerName = document.getElementById("burgerName");
+var calculate = document.getElementById("buttonCalculate");
+
+
+// click
+
+calculate.addEventListener("click",
+  function() {
+    alert("funziona")
+  }
+);
+//condition if burgerName is empty
+// if (burgerName.value == "") {
+//   alert("Before place your order, you must enter Your burger name!!")
+// }else{
+//   alert("ok")
+// }
